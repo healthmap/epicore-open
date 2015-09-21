2190cc21260c51893748e420cb2c2f8c1b441a0b
Jeff Andre <jeff.andre@childrens.harvard.edu>
2015-09-21 08:51:08 -0400

    removed epicor_test

--- scripts/da.ini.php
+++ scripts/da.ini.php
@@ -7,16 +7,13 @@
 
 [epicore_db]
 phptype = mysqli
-;hostspec = hmdb.cqhqr28vovrx.us-east-1.rds.amazonaws.com
 hostspec = healthmap.ctnhqqf4qsxf.us-east-1.rds.amazonaws.com
 username = hm
 password = 5k6n7q8u9p
-;database = epicore
-database = epicore_test
+database = epicore
 
 [hm]
 phptype = mysqli
-;hostspec = hmdb.cqhqr28vovrx.us-east-1.rds.amazonaws.com
 hostspec = healthmap.ctnhqqf4qsxf.us-east-1.rds.amazonaws.com
 username = hm
 password = 5k6n7q8u9p
