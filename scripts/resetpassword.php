16b4a6d46cd6381991359d8540206604375e08e7
Jeff Andre <jeff.andre@childrens.harvard.edu>
2015-10-21 19:15:03 -0400

    added reset password

---
+++ scripts/resetpassword.php
@@ -0,0 +1,24 @@
+<?php
+/* takes input from reset password form and sends an email to reset the fetp password */
+$formvars = json_decode(file_get_contents("php://input"));
+require_once "UserInfo.class.php";
+require_once "send_email.php";
+
+// get user email
+$user_email = strip_tags($formvars->username);
+$fetp_id = UserInfo::getFETPid($user_email);
+$userinfo = UserInfo::getUserInfo($user_email);
+
+// send email to reset password
+if ($fetp_id) {
+    $action = 'resetpassword';
+    sendMail($user_email, $userinfo['firstname'], "EpiCore Reset Password", $action, $fetp_id);
+    $status = 'success';
+}
+else{
+    $status = 'failed';
+}
+
+print json_encode(array('status' => $status));
+
+?>
