e0e6f788d00cfc76473e0550bcbf0565c1ccce67
Jeff Andre <jeff.andre@childrens.harvard.edu>
2015-12-07 15:13:21 -0500

    add markers on map for members

--- scripts/getmarkers.php
+++ scripts/getmarkers.php
@@ -6,11 +6,21 @@ get lat/lons of FETPs to show on a map - only allow this for superusers
 $formvars = json_decode(file_get_contents("php://input"));
 
 require_once "const.inc.php";
+require_once 'db.function.php';
+
 $fetps = array();
 
+$db = getDB();
+$all_fetps = $db->getAll("select * from fetp");
+
+
 // push the centerlat and centerlon onto fetp array for the center marker
+// and add all fetps to array
 if(is_numeric($formvars->centerlat) && is_numeric($formvars->centerlon)) {
     array_push($fetps, array("id" => 0, "icon" => "img/you.png", "latitude" => $formvars->centerlat, "longitude" => $formvars->centerlon, "show" => true, "title" => "Event Location"));
+    foreach($all_fetps as $fetp){
+        array_push($fetps, array("id" =>$fetp['fetp_id'], "icon" => "img/member.png", "latitude" => $fetp['lat'], "longitude" => $fetp['lon'], "show" => true, "title" => "Event Location"));
+    }
     print json_encode(array('status' => 'success', 'markers' => $fetps));
     exit;
 }
