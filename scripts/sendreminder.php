ca8471bf1e1859ef3df72817849e013f562e12e9
Jeff Andre <jeff.andre@childrens.harvard.edu>
2016-01-07 14:19:00 -0500

    updated reminder email templates

--- scripts/sendreminder.php
+++ scripts/sendreminder.php
@@ -17,8 +17,9 @@ $maillist = '';
 if ($action == 'preapprove_reminder') {
     $maillist = $db->getAll("select fetp_id, f.email,firstname from fetp as f, maillist as m  where f.email=m.email and active='N' and status='A' and pword_hash is null");
     foreach ($maillist as $member) {
-        sendMail($member['email'], $member['firstname'], "Reminder | EpiCore misses you! Set your password now!", $action, $member['fetp_id']);
+        sendMail($member['email'], $member['firstname'], "Reminder | You're almost there!", $action, $member['fetp_id']);
     }
+    sendMail('info@epicore.org', 'Info', "Reminder | You're almost there!", $action, '0');
 }
 
 // get accepted members with no password
@@ -26,8 +27,9 @@ if ($action == 'setpassword_reminder') {
     $maillist = $db->getAll("select fetp_id, f.email,firstname from fetp as f, maillist as m  where f.email=m.email and active='N'
                             and status='P' and pword_hash is null");
     foreach ($maillist as $member) {
-        sendMail($member['email'], $member['firstname'], "Reminder | EpiCore misses you! Set your password now!", $action, $member['fetp_id']);
+        sendMail($member['email'], $member['firstname'], "Reminder | You're almost there!", $action, $member['fetp_id']);
     }
+    sendMail('info@epicore.org', 'Info', "Reminder | You're almost there!", $action, '0');
 }
 
 // accepted members (with password set) that did not finish training
@@ -38,14 +40,16 @@ if ($action == 'training_reminder') {
     foreach ($maillist as $member) {
         sendMail($member['email'], $member['firstname'], "Reminder | 80% of success is showing up!", $action, $member['fetp_id']);
     }
+    sendMail('info@epicore.org', 'Info', "Reminder | 80% of success is showing up!", $action, '0');
 }
 
 // original launch members that did not apply
 if ($action == 'launch_reminder') {
     $maillist = $db->getAll("select maillist_id,email,firstname from contactlist where email not in (select email from maillist)");
     foreach ($maillist as $member) {
-        sendMail($member['email'], $member['firstname'], "Reminder | EpiCore Project Official Launch!", $action, $member['maillist_id']);
+        sendMail($member['email'], $member['firstname'], "Reminder | We miss you!", $action, $member['maillist_id']);
     }
+    sendMail('info@epicore.org', 'Info', "Reminder | We miss you!", $action, '0');
 }
 
 // return all applicants
