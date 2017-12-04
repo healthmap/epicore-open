3eb6ec04a769a376dd7da6b204afd7650079a4ca
Jeff Andre <jeff.andre@childrens.harvard.edu>
2017-12-04 14:35:16 -0500

    added comma between location and data in title

--- js/app/controllers2.js
+++ js/app/controllers2.js
@@ -474,7 +474,7 @@ controller('requestController2', function($rootScope, $window, $scope, $routePar
                 $scope.rfiData.event_location = getLocation();
                 $scope.rfiData.event_population = getPopulation();
                 $scope.rfiData.event_conditions = getConditions();
-                $scope.rfiData.event_title = $scope.rfiData.event_population + ', ' + $scope.rfiData.event_conditions + ' - ' + $scope.rfiData.event_location + $scope.rfiData.location.event_date;
+                $scope.rfiData.event_title = $scope.rfiData.event_population + ', ' + $scope.rfiData.event_conditions + ' - ' + $scope.rfiData.event_location + ', ' + $scope.rfiData.location.event_date;
                 $scope.rfiData.event_purpose = getPurpose();
                 $scope.rfiData.event_source = getSource();
                 $location.path('/sendrequest');
