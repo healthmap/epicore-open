b25017856770780a017b842fbc24f45f7a8be791
Lakshmi Yajurvedi <lakshmi.yajurvedi@childrens.harvard.edu>
2021-04-05 13:50:28 -0400

    rename attributes in .env for easy access

--- scripts/const.inc.php
+++ scripts/const.inc.php
@@ -7,14 +7,14 @@ $dotenv->load();
 $environment = $_ENV['environment'];
 $approval_username = $_ENV['approval_username'];
 $$approval_username = $_ENV['approval_password'];
-$key = $_ENV['key'];
-$clientid =  $_ENV['clientid'];
+$key = $_ENV['googleapi-key'];
+$clientid =  $_ENV['googleapi-clientid'];
 $fcm_server_key = $_ENV['fcm_server_key'];
 $fcm_send_url = $_ENV['fcm_send_url'];
 
 $tephinet_baseurl = $_ENV['tephinet_baseurl'];
-$consumerkey = $_ENV['consumerkey'];
-$consumersecret = $_ENV['consumersecret'];
+$consumerkey = $_ENV['tephinet-consumerkey'];
+$consumersecret = $_ENV['tephinet-consumersecret'];
 
 $userids = $_ENV['superusers_userids'];
 
