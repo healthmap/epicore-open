13cd99ece1635d916534281f36392132c115a64b
Zbigi Man <zbigi@zbigiman.com>
2021-04-20 13:26:40 +0200

    [#133][#156] Events API - error handling, remove uid form API calls

---
+++ scripts/ErrorController3.php
@@ -0,0 +1,44 @@
+<?php
+
+class ErrorController
+{
+
+  public static function getErrorMessage($error)
+    {
+        $action = null;
+        if (isset($_REQUEST['action'])) {
+            $action = $_REQUEST['action'];
+        }
+
+        $error_response = Array(
+            'error' => true
+        );
+
+        $error_messages = self::getErrorMessages();
+
+        if (isset($action) && isset($error_messages[$action])) {
+            $error_response["error_message"] = $error_messages[$action];
+        } else {
+            $error_response["error_message"] = $error_messages['default'];
+        }
+
+        $error_response["error_details"] = '<i>"'. $error->getMessage() . '"</i>' . ' in file <i>' . $error->getFile() .' </i>, line ' . $error->getLine() . '.';
+        return $error_response;
+    }
+
+    private static function getErrorMessages()
+    {
+        $path = __DIR__ . '/../constants/error-messages.json';
+
+        if (!file_exists($path)) {
+            return null;
+        }
+
+        $data = file_get_contents($path);
+
+        if (isset($data)) {
+            return json_decode($data, true);
+        }
+    }
+
+}
--- scripts/EventsAPI3.php
+++ scripts/EventsAPI3.php
@@ -1,11 +1,14 @@
 <?php
 require_once 'EventsController3.class.php';
+require_once 'ErrorController3.php';
+
 use EventsController as controller;
+use ErrorController as errorController;
 
 try {
   $output = controller::resolveRequest();
 } catch (Error $error) {
-  $output = controller::getErrorMessage($error);
+  $output = errorController::getErrorMessage($error);
 }
 
 echo json_encode($output);
