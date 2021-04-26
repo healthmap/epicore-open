9b7e2ba2b46e3fab29a18799378e04e52a2475ea
Lakshmi Yajurvedi <lakshmi.yajurvedi@childrens.harvard.edu>
2021-04-26 13:21:10 -0400

    eslint reco changes

--- cypress/tests/e2e/integration/_test_1_home-page-specs.js
+++ cypress/tests/e2e/integration/_test_1_home-page-specs.js
@@ -1,9 +1,8 @@
 describe('Home Page-Specs', () => {
-    const baseUrl = Cypress.config().baseUrl;
-    it('should display the app name on the home page', () => {
-      cy.visit(baseUrl+'home/'); // go to the home page
-  
-      cy.window()
+  const baseUrl = Cypress.config().baseUrl;
+  it('should display the app name on the home page', () => {
+    cy.visit(baseUrl + 'home/'); // go to the home page
+    cy.window()
       .then(win => {
         console.log('got app window object', win);
         return win;
@@ -11,12 +10,12 @@ describe('Home Page-Specs', () => {
       .its('angular')
       .then(ng => {
         console.log('got angular object', ng.version);
-      })
+      });
 
-      cy.get('body')
-      .should('have.attr', 'ng-app' , 'EpicoreApp');
+    cy.get('body')
+      .should('have.attr', 'ng-app', 'EpicoreApp');
 
-    });
-   
+  });
+
 });
 
--- cypress/tests/e2e/integration/_test_2_home-page-public-pages.js
+++ cypress/tests/e2e/integration/_test_2_home-page-public-pages.js
@@ -1,58 +1,39 @@
 describe('Home Page-public-pages', () => {
-    
-    // const baseUrl = Cypress.config().baseUrl;
-    // before(() => {
-    //     cy.visit(baseUrl); // go to the home page
-    // }); 
 
-    it('/about page navigation and content checks', () => {
-        
-        cy.visit('/about');
-        
-        cy.get('h3')
-        .contains('EpiCore is a system that aims to complement existing surveillance methods and speed up the process of finding, reporting and verifying public health events.');
+  it('/about page navigation and content checks', () => {
+    cy.visit('/about');
+    cy.get('h3')
+      .contains('EpiCore is a system that aims to complement existing surveillance methods and speed up the process of finding, reporting and verifying public health events.');
+  });
 
-        //Many more tags to check...
-        
-    });
+  it('/news page navigation and content checks', () => {
+    cy.visit('/news');
+    cy.get('h3')
+      .contains('This quarterly newsletter is an effort to keep EpiCore members informed about how EpiCore is doing, updates on the system, and any training or educational opportunities available.');
 
+  });
 
-    it('/news page navigation and content checks', () => {
+  it('/how page navigation and content checks', () => {
+    cy.visit('/how');
+    cy.get('h3')
+      .contains('EpiCore links a worldwide member network of health professionals in order to provide verification of suspected or rumored disease outbreaks.');
 
-        cy.visit('/news');
-        
-        cy.get('h3')
-        .contains('This quarterly newsletter is an effort to keep EpiCore members informed about how EpiCore is doing, updates on the system, and any training or educational opportunities available.');
+  });
 
-    });
+  it('/who page navigation and content checks', () => {
+    cy.visit('/who');
+    cy.get('p')
+      .contains('All health professionals with formal education and training in animal or human health, and knowledge of basic principles of epidemiology, infectious disease or related fields are encouraged to apply online.');
 
-    it('/how page navigation and content checks', () => {
-
-        cy.visit('/how');
-        
-        cy.get('h3')
-        .contains('EpiCore links a worldwide member network of health professionals in order to provide verification of suspected or rumored disease outbreaks.');
-
-    });
-
-    it('/who page navigation and content checks', () => {
-
-        cy.visit('/who');
-        
-        cy.get('p')
-        .contains('All health professionals with formal education and training in animal or human health, and knowledge of basic principles of epidemiology, infectious disease or related fields are encouraged to apply online.');
-
-    });
+  });
 
 
-    it('/events_public page navigation and content checks', () => {
-
-        cy.visit('/events_public');
-        cy.get('span')
-        .contains('Public RFI List');
-        
-        cy.get('select').select('Most Recent');
-    });
+  it('/events_public page navigation and content checks', () => {
+    cy.visit('/events_public');
+    cy.get('span')
+      .contains('Public RFI List');
+    cy.get('select').select('Most Recent');
+  });
 
 });
 
