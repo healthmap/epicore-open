5d790bf14d011b8cf6e6f96753194c8be6e8432d
Jakub Bartkiewicz <jakub.bartkiewicz@htdevelopers.com>
2021-06-21 17:47:21 +0200

    CognitoException

--- scripts/Exception/CognitoException.php
+++ scripts/Exception/CognitoException.php
@@ -5,6 +5,9 @@ if (file_exists("/usr/share/php/vendor/autoload.php")) {
 }
 require_once '../vendor/autoload.php';
 
+use Aws\Credentials\CredentialProvider;
+use Aws\Exception;
+
 /**
  * Class CognitoException
  */
