f124b67ec301e3c0f92a1cb5bb1f5a789db96b14
Lakshmi Yajurvedi <lakshmi.yajurvedi@childrens.harvard.edu>
2021-07-29 12:48:05 -0400

    changes to html and uncommenting Congratulations email

--- scripts/UserInfo.class.php
+++ scripts/UserInfo.class.php
@@ -871,7 +871,7 @@ class UserInfo
 
                 if ($status == 'approved') {
                     $fetp_id = UserInfo::getFETPid($approve_email);
-                 //   sendMail($approve_email, $approve_name, "Congratulations!", $status, $fetp_id);
+                   sendMail($approve_email, $approve_name, "Congratulations!", $status, $fetp_id);
                 }
             }
             else if ($status == 'declined') {
