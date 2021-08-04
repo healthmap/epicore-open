90db96bea00e6f1e1da8e0ed7ee5473ca76e140e
Jakub Bartkiewicz <jakub.bartkiewicz@htdevelopers.com>
2021-08-04 20:38:54 +0200

    issue-288 save name in cognito

--- scripts/Service/IAuthService.php
+++ scripts/Service/IAuthService.php
@@ -8,7 +8,7 @@ interface IAuthService
     public function ValidToken(string $token);
     public function RevokeToken(string $username);
     public function LoginUser(string $username, string $password);
-    public function SingUp(string $username, string $password, bool $dontSendEmail = false , bool $dontUpdatePassword = false , string $name);
+    public function SingUp(string $username, string $password, bool $dontSendEmail = false , bool $dontUpdatePassword = false);
     public function UpdatePassword(string $username , string $password , string $code);
     public function ForgotPassword(string $username);
     public function ConfirmAccount(string $username , string $password , string $newPassword);
