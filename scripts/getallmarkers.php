ed0f9946d9e28e468e40ccfdfa0b3aab924fe208
Lakshmi Yajurvedi <lakshmi.yajurvedi@childrens.harvard.edu>
2021-09-01 12:34:57 -0400

    updated pins for animal vs other experts

--- scripts/getallmarkers.php
+++ scripts/getallmarkers.php
@@ -9,14 +9,23 @@ require_once 'db.function.php';
 $fetps = array();
 
 $db = getDB();
-$all_fetps = $db->getAll("select * from fetp where active='Y' and status='A'");
+$all_fetps = $db->getAll("select m.maillist_id, m.animal_health, m.human_health, m.env_health, f.*
+                            FROM fetp f
+                            JOIN maillist m ON m.maillist_id = f.maillist_id
+                            WHERE f.active='Y' 
+                            AND f.status='A'");
 
 $country_members = array();
 $std_countries = unserialize(COUNTRIES);
 // add all fetps to array
 foreach($all_fetps as $fetp){
     $country = $std_countries[$fetp['countrycode']];
-    array_push($fetps, array("id" =>$fetp['fetp_id'], "icon" => "img/member.png", "latitude" => $fetp['lat'], "longitude" => $fetp['lon'], "country" => $country, "show" => true, "title" => "Member Location"));
+    // array_push($fetps, array("id" =>$fetp['fetp_id'], "icon" => "img/member.png", "latitude" => $fetp['lat'], "longitude" => $fetp['lon'], "country" => $country, "show" => true, "title" => "Member Location"));
+    if(is_numeric($fetp['animal_health']) == 1) { //animal_health expertise
+        array_push($fetps, array("id" =>$fetp['fetp_id'], "icon" => "img/member_animalHealthExp.png", "latitude" => $fetp['lat'], "longitude" => $fetp['lon'], "country" => $country, "show" => true, "title" => "Member Location", "animalExp" => $fetp['animal_health']));
+    } else {
+        array_push($fetps, array("id" =>$fetp['fetp_id'], "icon" => "img/member.png", "latitude" => $fetp['lat'], "longitude" => $fetp['lon'], "country" => $country, "show" => true, "title" => "Member Location", "animalExp" => $fetp['animal_health']));
+    }
     if ($country){
         if ($country_members[$country] >= 1)
             $country_members[$country]++;
--- scripts/getmarkers.php
+++ scripts/getmarkers.php
@@ -11,18 +11,43 @@ require_once 'db.function.php';
 $fetps = array();
 
 $db = getDB();
-$all_fetps = $db->getAll("select * from fetp where active='Y' and status='A'");
-$all_locations = $db->getAll("SELECT * FROM member_location");
+$all_fetps = $db->getAll("select m.maillist_id, m.animal_health, m.human_health, m.env_health, f.*
+                            FROM fetp f
+                            JOIN maillist m ON m.maillist_id = f.maillist_id
+                            WHERE f.active='Y' 
+                            AND f.status='A'");
+$all_locations = $db->getAll("SELECT ml.maillist_id, ml.animal_health, ml.human_health, ml.env_health, f.fetp_id, m.*
+                                FROM member_location m
+                                JOIN fetp f ON f.fetp_id = m.fetp_id
+                                JOIN maillist ml ON ml.maillist_id = f.maillist_id");
 
 // push the centerlat and centerlon onto fetp array for the center marker
 // and add all fetps to array
 if(is_numeric($formvars->centerlat) && is_numeric($formvars->centerlon)) {
-    array_push($fetps, array("id" => 0, "icon" => "img/you.png", "latitude" => $formvars->centerlat, "longitude" => $formvars->centerlon, "show" => true, "title" => "Event Location"));
+    array_push($fetps, array("id" => 0, "icon" => "img/you.png", "latitude" => $formvars->centerlat, "longitude" => $formvars->centerlon, "show" => true, "title" => "Event Location", "animalExp" => "UNK"));
     foreach($all_fetps as $fetp){
-        array_push($fetps, array("id" =>$fetp['fetp_id'], "icon" => "img/member.png", "latitude" => $fetp['lat'], "longitude" => $fetp['lon'], "show" => true, "title" => "Event Location"));
+        if(is_numeric($fetp['animal_health']) == 1) { //animal_health expertise
+            array_push($fetps, array("id" =>$fetp['fetp_id'], "icon" => "img/member_animalHealthExp.png", "latitude" => $fetp['lat'], "longitude" => $fetp['lon'], "show" => true, "title" => "Event Location", "animalExp" => $fetp['animal_health']));
+        } else {
+            array_push($fetps, array("id" =>$fetp['fetp_id'], "icon" => "img/member.png", "latitude" => $fetp['lat'], "longitude" => $fetp['lon'], "show" => true, "title" => "Event Location", "animalExp" => $fetp['animal_health']));
+        }
     }
+
     foreach($all_locations as $location){
-        array_push($fetps, array("id" =>$location['location_id'], "icon" => "img/member.png", "latitude" => $location['lat'], "longitude" => $location['lon'], "show" => true, "title" => "Event Location"));
+        // array_push($fetps, array("id" =>$location['location_id'], "icon" => "img/member.png", "latitude" => $location['lat'], "longitude" => $location['lon'], "show" => true, "title" => "Event Location" "animalExp" => ""));
+        $found = false;
+        foreach($all_fetps as $fetp){
+            if($location['fetp_id'] == ($fetp['fetp_id'])) { 
+                if(is_numeric($fetp['animal_health']) == 1) { //animal_health expertise
+                    $found = true;
+                    array_push($fetps, array("id" =>$location['location_id'], "icon" => "img/member_animalHealthExp.png.png", "latitude" => $location['lat'], "longitude" => $location['lon'], "show" => true, "title" => "Event Location", "animalExp" => $fetp['animal_health']));
+                    break;
+                }
+            }
+        }
+        if(!$found) {
+            array_push($fetps, array("id" =>$location['location_id'], "icon" => "img/member.png", "latitude" => $location['lat'], "longitude" => $location['lon'], "show" => true, "title" => "Event Location", "animalExp" => "0"));
+        }
     }
     print json_encode(array('status' => 'success', 'markers' => $fetps));
     exit;
